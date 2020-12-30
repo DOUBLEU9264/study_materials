5a8d767eb3949f6123ce18747c58a8c9cdb57b59
3RM1N3 <wangyu7439@gmail.com>
2020-12-30 20:00:46 +0800

    更新药理

--- 药理学/练习册/处理yaoli.py
+++
@@ -1,9 +0,0 @@
-with open('yaoli.txt', 'r', encoding='UTF-8') as f1, \
-    open('yaoli处理后.txt', 'w', encoding='UTF-8') as f2:
-    
-    lines = f1.readlines()
-    for i in range(len(lines)-1):
-        if len(lines[i+1].strip())>0 and lines[i+1].strip()[0] not in '123456789ABCDEFGHIJ':
-            f2.write(lines[i].strip())
-        else:
-            f2.write(lines[i].strip() + '\n')
